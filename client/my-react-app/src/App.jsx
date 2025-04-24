4bb5696ab0b0f1e8ac3c7ae23a60ccc458fe0181
Keit Koki <kokikeith@gmail.com>
2025-04-25 01:55:01 +0300

    updated the delete button functionality

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -57,7 +57,7 @@ function App() {
       });
       const data = await response.json();
       setBooks((prev) => prev.map((book) => {
-        if (book.id === pk) {
+        if (book.id == pk) {
           return data;
         } else {
           return book;
@@ -67,6 +67,17 @@ function App() {
       console.log(err);
     }
   };
+
+  const deleteBook = async (pk) => {
+    try{
+      const response = await fetch (`http://127.0.0.1:8000/api/books/${pk}`, {
+      method: "DELETE",
+      });
+      setBooks((prev) => prev.filter((book) => book.id !== pk));
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   return (
     <>
@@ -95,7 +106,10 @@ function App() {
             placeholder='New Title...'
             onChange={(e) => setNewTitle(e.target.value)}
           />
-          <button onClick={() => updateTitle(book.id, book.releaseYear)}> Change Title </button>
+          <button onClick={() => updateTitle(book.id, book.release_year)}> 
+            Change Title 
+          </button>
+          <button onClick={() => deleteBook(book.id)}>Delete</button>
         </div>
       ))}
     </>
