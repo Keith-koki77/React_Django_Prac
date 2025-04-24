f960fa0e8359cb346d965a967dfa54f90eb4eb65
Keit Koki <kokikeith@gmail.com>
2025-04-25 01:18:40 +0300

    Update change function in App.js

--- client/my-react-app/src/App.jsx
+++ client/my-react-app/src/App.jsx
@@ -6,6 +6,8 @@ function App() {
   const [title, setTitle] = useState("");
   const [releaseYear, setReleaseYear] = useState(0);
 
+  const [newTitle, setNewTitle] = useState("");
+
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -40,6 +42,32 @@ function App() {
     }
   };
 
+  const updateTitle = async (pk, release_year) => {
+    const bookData = {
+      title: newTitle,
+      release_year,
+    };
+    try{
+      const response = await fetch (`http://127.0.0.1:8000/api/books/${pk}`, {
+      method: "PUT",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(bookData),
+      });
+      const data = await response.json();
+      setBooks((prev) => prev.map((book) => {
+        if (book.id === pk) {
+          return data;
+        } else {
+          return book;
+        }
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <h1> Book Website </h1>
@@ -62,8 +90,12 @@ function App() {
           {" "}
           <p>Title: {book.title}</p>
           <p>Release Year: {book.release_year}</p>
-          <input type='text' placeholder='New Title...' />
-          <button> Change Title </button>
+          <input 
+            type='text' 
+            placeholder='New Title...'
+            onChange={(e) => setNewTitle(e.target.value)}
+          />
+          <button onClick={() => updateTitle(book.id, book.releaseYear)}> Change Title </button>
         </div>
       ))}
     </>
