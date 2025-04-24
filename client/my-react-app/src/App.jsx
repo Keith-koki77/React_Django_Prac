3b47b0f884c62365960f68bbac21cec4fca804b3
Keit Koki <kokikeith@gmail.com>
2025-04-24 16:43:09 +0300

    Installed and updated

---
+++ client/my-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1> Book Website </h1>
+
+      <div>
+        <input type='text' placeholder='Book Title...' />
+        <input type='number' placeholder='Release Date...' />
+        <button> Add Book </button>
+      </div>
+    </>
+  );
+}
+
+export default App
